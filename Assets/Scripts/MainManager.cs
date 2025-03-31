789933db294553f4233470474e60fa2aaab62679
SJ9211 <jsj921127@naver.com>
2025-03-31 15:50:39 +0900

    New

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,21 +11,22 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text HighScore;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -36,6 +37,7 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+        AddPoint(0);
     }
 
     private void Update()
@@ -65,7 +67,12 @@ public class MainManager : MonoBehaviour
     void AddPoint(int point)
     {
         m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        ScoreText.text = $"Score :{GameManager.Instance.Name} : {m_Points}";
+        if ( GameManager.Instance.HighScore <= m_Points)
+        {
+            GameManager.Instance.HighScore = m_Points;
+            HighScore.text = $"Best Score : {GameManager.Instance.bestPlayer} : {m_Points}" ;
+        }
     }
 
     public void GameOver()
